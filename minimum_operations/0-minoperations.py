397f994d447f7d958239b22207f839b5841e27d3
gasanadj <d.gasana@alustudent.com>
2023-09-10 01:23:24 +0200

    minimum operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''minimun operations'''
+
+
+def minOperations(n):
+    if n == 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            n //= divisor
+            operations += divisor
+        else:
+            divisor += 1
+
+    return operations
